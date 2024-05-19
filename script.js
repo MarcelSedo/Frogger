c70098e83fa43696a382d057cfd090ed417d57ff
MarcelSedo <marcel.sedo1@gmail.com>
2024-05-19 10:07:36 +0200

    Pridávame javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+//1. nastavenie štyroch konštánt
+
+const timeLeftDisplay = document.querySelector("#time-left")
+const resultDisplay = document-querySelector("#result")
+const startPauseButton = document.querySelector("#start-pause-button")
+const squares = document.querySelectorAll(".grid div")
+//2. vždy keď sa pohneme, eventListener to zaregistruje a spustí funkciumoveFrog
+function moveFrog() {
+    //3.squares dávam do array
+}
+document.addEventListener("keyup", moveFrog)
