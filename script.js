65b8ffec12b3b6b263dba1c6d6a17ef8f312ada9
MarcelSedo <marcel.sedo1@gmail.com>
2024-05-20 11:39:02 +0200

    Nastavujem pohyb u log-right v .js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 const logsLeft = document.querySelectorAll(".log-left")
-const logsLeft = document.querySelectorAll(".log-left")git
+const logsRight = document.querySelectorAll(".log-right")
 //2. vždy keď sa pohneme, eventListener to zaregistruje a spustí funkciumoveFrog
 console.log(squares)
 let currentIndex = 76
@@ -39,11 +39,10 @@ document.addEventListener("keyup", moveFrog)
 
 // 5. funkcia, ktorá vyberá logy v dive
 function autoMoveLogs() {
-    logsLeft.forEach(logLeft => moveLogLeft(logLeft) )
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    //8. pridávame do funkcie foreach pre right
+    logsRight.forEach(logRight => moveLogRight(logRight))
 }
-
-//6. 
-autoMoveLogs
 // 4. funkcia, ktorá nastavuje pohyb tilov do strany
 
 function moveLogLeft(logLeft) {
@@ -61,11 +60,33 @@ function moveLogLeft(logLeft) {
             logLeft.classList.remove("l4")
             logLeft.classList.add("l5")
         case logLeft.classList.contains("l5") :
-                logLeft.classList.remove("l5")
-                logLeft.classList.add("l1")
+            logLeft.classList.remove("l5")
+            logLeft.classList.add("l1")
 
     }
 }
+//8. kopírujem funkciu vyššie a nastavujem pohyb logov doprava
+function moveLogRight(logRight) {
+    switch(true) {
+        case logRight.classList.contains("l1") :
+            logRight.classList.remove("l1")
+            logRight.classList.add("l5")
+        case logRight.classList.contains("l2") :
+            logRight.classList.remove("l2")
+            logRight.classList.add("l1")
+        case logRight.classList.contains("l3") :
+            logRight.classList.remove("l3")
+            logRight.classList.add("l2")
+        case logRight.classList.contains("l4") :
+            logRight.classList.remove("l4")
+            logRight.classList.add("l3")
+        case logRight.classList.contains("l5") :
+            logRight.classList.remove("l5")
+            logRight.classList.add("l4")
+
+    }
+}
+
 
 //7. nastavujeme interval pohybu
 setInterval(autoMoveLogs, 1000)
